4b354fb6fff42b098cf26bd1eb8fbf725b78c2e3
Mudassir <muddasirali99@gmail.com>
2022-10-06 14:16:48 +0500

    w

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from "react";
 import { JsxElement } from "typescript";
 interface Props {
-  children?:JSX.Element|any;
-  className?:string;
-  variant?:string;
-  as?:any; 
-  href?:string; 
+  children?: JSX.Element | any;
+  className?: string;
+  variant?: string;
+  as?: any;
+  href?: string;
+  restProps?: any;
 }
 const variantClasses = {
   h1: "font-bold lg:text-[45px] xl:text-[56px] text-[64px] 3xl:text-[76px]",
@@ -14,31 +15,30 @@ const variantClasses = {
   h4: "font-bold lg:text-[14px] xl:text-[17px] text-[20px] 3xl:text-[24px]",
   h5: "lg:text-[11px] xl:text-[14px] text-[16px] 3xl:text-[19px]",
   h6: "xl:text-[12px] text-[14px] 3xl:text-[16px] lg:text-[9px]",
-
 };
-const Text: FC<Props> = ({ children, className, variant, as, ...restProps }) => {
-  console.log(as);
-  const Component = as;
-  if (!Component) return null;
-  if (variant != undefined || null) {
+
+//  children, className, variant, as, ...restProps
+const Text: FC<Props> = (props) => {
+  const Component = props.as;
+  if (props.variant != undefined || null) {
     return (
       <>
-        <Component 
-          className={`${className} ${variantClasses[variant as keyof typeof variantClasses]}`}
-          {...restProps}
+        <Component
+          className={`${props.className} ${variantClasses[props.variant as keyof typeof variantClasses]}`}
+          {...props.restProps}
         >
-          {children}
+          {props.children}
         </Component>
       </>
-      );
+    );
   } else {
 
-  return (
-    <>
-  
-    </>
+    return (
+      <>
+
+      </>
     );
-    
+
   }
 
 
