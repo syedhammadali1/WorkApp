5adc5ba9fa7c9bf6be543caad207fa03db21295c
syedhammadali1 <hammadali407292@gmail.com>
2022-10-06 15:02:58 +0500

    list comp

---
+++ src/components/List/index.tsx
@@ -0,0 +1,22 @@
+// import React from "react";
+import React, { FC } from "react";
+
+interface Props {
+  children?:any;
+  className?:string;
+  orientation?:string;
+  restProps?:any;
+}
+
+export const List:FC<Props> = (props) => {
+  return (
+    <div
+      className={`${props.className} ${
+        props.orientation === "horizontal" ? "common-grid" : "common-list"
+      }`}
+      {...props.restProps}
+    >
+      {props.children}
+    </div>
+  );
+};
