ffa5d8e3c6a6dd634bf0344150f1fa1977a69299
Mudassir <muddasirali99@gmail.com>
2022-10-13 13:18:23 +0500

    w

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -18,61 +18,122 @@ const Header: FC<Props> = (props) => {
     return newUrl === navLink ? activeClassName : "";
   }
 
+  <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 w-[100%]">
+    <div className="container flex flex-wrap justify-between items-center mx-auto">
+      <a href="https://flowbite.com/" className="flex items-center">
+        <img
+          src="https://flowbite.com/docs/images/logo.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="Flowbite Logo"
+        />
+        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          Flowbite
+        </span>
+      </a>
+      <button
+        data-collapse-toggle="navbar-default"
+        type="button"
+        className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-default"
+        aria-expanded="false"
+      >
+        <span className="sr-only">Open main menu</span>
+        <svg
+          className="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <li>
+            <a
+              href="#"
+              className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+              aria-current="page"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              Services
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              Pricing
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
   return (
     <>
-      <Row className="font-almarai items-start w-[100%]">
+      <Row className="font-almarai items-start w-full">
         <Img
           src="images/img_worqapplogo2.png"
-          className="WorqappLogoTwo"
+          className="WorqappLogoTwo mr-3 h-16 sm:h-16"
           alt="WorqappLogoTwo"
         />
         <Text
-          className="mb-[1px] mt-5 w-[auto] cursor-[pointer] "
+          className="hidden lg:block mb-[1px] mt-5 w-[auto] cursor-[pointer] "
           as="a" href="#usecase"
           variant="h3"
         >
           WorqApp
         </Text>
         <Row className="items-start justify-center my-[auto] mx-[auto] w-[90%]">
-          <NavLink className={navClass('/')} to="/" >
-            <Text className={"hover:text-pink_400 hover:font-bold my-[auto] mx-[15px] w-[auto]"} as="h5" variant="h5">
-              Home
-            </Text>
-          </NavLink>
-          <NavLink className={navClass('/w')} to="/w" >
-            <Text className={"hover:text-pink_400 hover:font-bold my-[auto] mx-[15px] w-[auto]"} as="h5" variant="h5">
-              w
-            </Text>
-          </NavLink>
-          
-          <NavLink className={navClass('/#usecase')} to="/#usecase">
-            <Text
-              className="hover:text-pink_400 hover:font-bold my-[auto] mx-[15px] w-[auto]"
-              as="h5"
-              variant="h5"
-            >
-              Use Cases
-            </Text>
-          </NavLink>
-          <NavLink className={navClass('/#features')} to="/#features">
-            <Text
-              className="  hover:text-pink_400 hover:font-bold my-[auto] mx-[15px] w-[auto]"
-              as="h5"
-              variant="h5"
-            >
-              Features
-            </Text>
-          </NavLink>
-          <NavLink className={navClass('/#pricing')} to="/#pricing">
-            <Text
-              className="  hover:text-pink_400 hover:font-bold my-[auto] mx-[15px] w-[auto]"
-              as="h5"
-              variant="h5"
-            >
-              Pricing
-            </Text>
-          </NavLink>
-
+          {[
+            ['Home', '/'],
+            ['Use Cases', '/#usecase'],
+            ['Features', '/#features'],
+            ['Pricing', '/#pricing'],
+          ].map(([title, url]) => (
+            <>
+              <NavLink className={navClass(url)} to={url}>
+                <Text
+                  className="hover:text-pink_400 hover:font-bold my-[auto] mx-[15px] w-[auto]"
+                  as="h5"
+                  variant="h5"
+                >
+                  {title}
+                </Text>
+              </NavLink>
+            </>
+          ))}
         </Row>
         <Button className="font-bold mx-[auto] my-[auto] text-center w-[10%]">
           Log In
