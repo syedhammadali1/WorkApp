1e4d9a87c9dc9a0c0de593f0682d91f7003dd878
syedhammadali1 <hammadali407292@gmail.com>
2022-10-20 19:12:27 +0500

    video

--- src/components/Section/Home/SectionOne.tsx
+++ src/components/Section/Home/SectionOne.tsx
@@ -69,7 +69,7 @@ export const SectionOne: FC<Props> = (props) => {
           >
             <Img
               src="images/img_play.svg"
-              className="h-[60px] mx-[auto] inset-x-[0] mt-[50px]  lg:h-[55px] xl:h-[68px] 2xl:h-[77px] 3xl:h-[92px]   lg:w-[54px] xl:w-[67px] 2xl:w-[76px] 3xl:w-[91px]  lg:mt-[118px] xl:mt-[144px] 2xl:mt-[160px] 3xl:mt-[185px]"
+              className="h-[60px] mx-[auto] inset-x-[0] mt-[50px]  lg:h-[55px] xl:h-[68px] 2xl:h-[77px] 3xl:h-[92px]   lg:w-[54px] xl:w-[67px] 2xl:w-[76px] 3xl:w-[91px]  lg:mt-[118px] xl:mt-[144px] 2xl:mt-[160px] 3xl:mt-[185px] hover:cursor-pointer"
               alt="play"
               clickEvent={ handleClick }
             />
@@ -78,7 +78,7 @@ export const SectionOne: FC<Props> = (props) => {
       </Column>
       <MyModal ref={childRef}  width={1000}  modalBodyStyle={{ height: 'auto' }} fullModalStyle={{ backgroundColor :'lightblue' }} restProps = {{  footer:null,closable:false}}>
             <video width="100%" controls>
-                <source src={'https://rr1---sn-hju7enel.c.drive.google.com/videoplayback?expire=1666283895&ei=N0FRY8f6NtLl1bYPnPKTUA&ip=27.96.93.111&cp=QVRLWENfUlhXRlhPOkp5MGNNNEFZRWNlTWg3VGN5VVVuWl9kSG5jQ08tdHdpTzFZYnZZQ3pkbE0&id=e7e55787bd294f94&itag=22&source=webdrive&requiressl=yes&mh=D6&mm=32&mn=sn-hju7enel&ms=su&mv=u&mvi=1&pl=24&ttl=transient&susc=dr&driveid=1KWsbqaeYzwx9fUhoW85f0cnwByIDFioA&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=29.953&lmt=1666143216466762&mt=1666268550&subapp=DRIVE_WEB_FILE_VIEWER&txp=0016224&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRgIhALOFFzcgVv7VOnxHPfgb-OgDWlM2LWJTxZBE5-FhmvCvAiEA9fd9N5CHSSZ5wDiWavENFBU0zJGMGrYDrL04LGrEWVY=&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIhAPK6LFV1ESblVW2SwBXqmNDyNpxU2wO4Yx8eZZ9GOBdtAiAEr964IEOqB-XS_TE0L01wdgpIiQr9Lph6Bcdmjsu-NQ==&cpn=WvQhkXeFxXtbnKlk&c=WEB_EMBEDDED_PLAYER&cver=1.20221018.01.00'} type="video/mp4"/>
+                <source src='video.mp4' type="video/mp4"/>
             </video>
       </MyModal>
 
