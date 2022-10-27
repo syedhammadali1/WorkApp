0378316909e08e66f370b99a937525ab85c155c8
Mudassir <muddasirali99@gmail.com>
2022-10-27 13:06:59 +0500

    w

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import "./styles/color.css";
 import "./styles/font.css";
 import ReactDOM from "react-dom";
 import App from "./App";
-import 'antd/dist/antd.css';
 import "./styles/index.css";
 import "./styles/tailwind.css";
+// import "antd/dist/antd.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
